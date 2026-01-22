776a9678f911f82315a169334cd8a9e39a7f1efc
shoaib-eth <shoaib.developer69@gmail.com>
2026-01-22 11:27:43 +0530

    Testing With Foundry: Gasless Token Transfer

---
+++ 2-foundry-test-course/test/GaslessTokenTransfer.t.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Test, console} from "forge-std/Test.sol";
+import {ERC20Permit} from "../src/ERC20Permit.sol";
+import {GaslessTokenTransfer} from "../src/app/GaslessTokenTransfer.sol";
+
+contract GaslessTokenTransferTest is Test {
+    ERC20Permit private token;
+    GaslessTokenTransfer private gasless;
+
+    uint256 constant SENDER_PRIVATE_KEY = 123;
+
+    address sender;
+    address reciever;
+    uint256 constant AMOUNT = 1000;
+    uint256 constant FEE = 10;
+
+    function setUp() public {
+        sender = vm.addr(SENDER_PRIVATE_KEY);
+        reciever = address(2);
+
+        token = new ERC20Permit("Hello", "HL", 18);
+        token.mint(sender, AMOUNT + Fee);
+        gasless = new GaslessTokenTransfer();
+    }
+
+    function testValidSig() public {
+        // prepare permit message
+        uint256 deadline = block.timestamp + 60;
+        bytes32 permitHash = _getPermitHash(sender, address(gasless), AMOUNT + FEE, token.nonces(sender), deadline);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(SENDER_PRIVATE_KEY, permitHash);
+
+        // execute send
+        gasless.send(
+            address(token),
+            sender,
+            receiver,
+            AMOUNT,
+            FEE,
+            deadline,
+            // Permit signature
+            v,
+            r,
+            s
+        );
+
+        // check token balances
+        assertEq(token.balanceOf(sender), 0, "Sender Balance");
+        assertEq(token.balanceOf(reciever), AMOUNT, "Reciever Balance");
+        assertEq(token.balanceOf(address(this)), FEE, "Fee");
+
+    }
+
+    function _getPermitHash(address owner, address spender, uint256 value, uint256 nonce, uint256 deadline)
+        private
+        view
+        returns (bytes32)
+    {
+        return keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                token.DOMAIN_SEPARATOR(),
+                keccak256(
+                    abi.encode(
+                        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"),
+                        owner,
+                        spender,
+                        value,
+                        nonce,
+                        deadline
+                    )
+                )
+            )
+        );
+    }
+}
